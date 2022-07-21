24ef4347eefd8e706694777c1806c2719082454b
maksimblankov <maxblankov436@gmail.com>
2022-07-21 14:10:50 +0300

    Made a call to one constructor from another

--- src/main/java/Lecture19_20/Lecture20.java
+++ src/main/java/Lecture19_20/Lecture20.java
@@ -24,11 +24,14 @@ class Rectangle {
     private final int sideD;
 
     public Rectangle(int sideA, int sideB, int sideC, int sideD) {
-
-        this.sideA = sideA;
-        this.sideB = sideB;
-        this.sideC = sideC;
-        this.sideD = sideD;
+        if (sideA > 0 && sideB > 0 && sideC > 0 && sideD > 0) {
+            this.sideA = sideA;
+            this.sideB = sideB;
+            this.sideC = sideC;
+            this.sideD = sideD;
+        } else {
+            throw new IllegalArgumentException("Invalid argument");
+        }
     }
 
     public Rectangle(Point a, Point b, Point c, Point d) {
